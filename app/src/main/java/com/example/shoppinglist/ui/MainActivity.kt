25d8d6a84cfd57b61a2b69636a6f6cc210357b7c
RonhoGroniko <sarapovrodion@gmail.com>
2025-07-26 21:24:09 +0200

    Add LinearLayout usage for shopList

--- app/src/main/java/com/example/shoppinglist/ui/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/ui/MainActivity.kt
@@ -1,23 +1,57 @@
 package com.example.shoppinglist.ui
 
 import android.os.Bundle
-import android.util.Log
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.shoppinglist.R
+import com.example.shoppinglist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var linearLayoutShopList: LinearLayout
     private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        linearLayoutShopList = findViewById(R.id.linearLayoutItems)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
-            Log.d("MainActivity", it.toString())
+            showList(it)
+        }
+    }
+
+    private fun showList(list: List<ShopItem>) {
+        linearLayoutShopList.removeAllViews()
+        for (item in list) {
+            val layoutId = when {
+                item.enabled -> {
+                    R.layout.item_shop_enabled
+                }
+                else -> {
+                    R.layout.item_shop_disabled
+                }
+            }
+            val view = LayoutInflater.from(this).inflate(
+                layoutId,
+                linearLayoutShopList,
+                false
+            )
+            val textViewName = view.findViewById<TextView>(R.id.textViewItemName)
+            val textViewCount = view.findViewById<TextView>(R.id.textViewItemCount)
+            textViewName.text = item.name
+            textViewCount.text = item.count.toString()
+            view.setOnLongClickListener {
+                viewModel.changeEnabledState(item)
+                true
+            }
+            linearLayoutShopList.addView(view)
         }
     }
 }
+
